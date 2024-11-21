872787138d7ec8ccb2993f15520ead6da3e7ae8a
eminokumus1 <eminokumus1@gmail.com>
2024-11-21 21:36:21 +0300

    Back button added to SignupActivity

--- app/src/main/java/com/eminokumus/learnconnect/signup/SignupActivity.kt
+++ app/src/main/java/com/eminokumus/learnconnect/signup/SignupActivity.kt
@@ -3,10 +3,21 @@ package com.eminokumus.learnconnect.signup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.eminokumus.learnconnect.R
+import com.eminokumus.learnconnect.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setBackButtonOnClickListener()
+    }
+
+    private fun setBackButtonOnClickListener(){
+        binding.backButton.setOnClickListener {
+            finish()
+        }
     }
 }
