b3dc3915e937af0091cf748433ba79dfb7ccda0d
eminokumus1 <eminokumus1@gmail.com>
2024-11-28 08:38:08 +0300

    Test dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
@@ -79,5 +78,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
 
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
+    testImplementation("org.mockito:mockito-inline:2.21.0")
+    testImplementation("org.mockito:mockito-core:5.12.0")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
 
 }
